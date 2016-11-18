3d7cb621af493cf0635dd850aedcb7e4e0c55a5d
Vlad Kunak <Vlad Kunak>
2016-11-18 17:58:06 +0200

    add catalog block

--- assets/js/app/formValidate.js
+++ assets/js/app/formValidate.js
@@ -5,13 +5,25 @@ $(document).ready(function () {
         email: {
           required: true,
           email: true
+        },
+        'test[]': {
+          required: true,
+          maxlength: 2
         }
       },
       messages: {
         email: {
           required: 'Корректный адрес email имеет формат author@mail.com.',
           email: 'Корректный адрес email имеет формат author@mail.com.'
+        },
+        'test[]': {
+          required: "You must check at least 1 box",
+          maxlength: "Check no more than {0} boxes"
         }
+      },
+      submitHandler: function (form) { // for demo
+        alert('valid form submitted'); // for demo
+        return false; // for demo
       }
     });
 });
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-    $('.subscribe__cat').select2();
+  $('.dropdown__state').click(function () {
+    console.log('15');
+    $('.dropdown__list').toggleClass("active","active");
+  });
+//     $('.subscribe__cat').select2();
 });
+
