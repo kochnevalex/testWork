7b20a9349962dd90151c84b9ad809d7f03118dca
Vlad Kunak <Vlad Kunak>
2016-11-25 14:15:11 +0200

    edit subscribe form

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-  $('.dropdown__state').click(function () {
-    console.log('15');
-    $('.dropdown__list').toggleClass("active","active");
-  });
-//     $('.subscribe__cat').select2();
+  // $('.dropdown__state').click(function () {
+  //   console.log('15');
+  //   $('.dropdown__list').toggleClass("active","active");
+  // });
+    $('.subscribe__cat').select2({
+      minimumResultsForSearch: Infinity
+    });
 });
 
