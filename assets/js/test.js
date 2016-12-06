de95fa311e9620a8f04fe508ca9e6ddc745c8d23
Vlad Kunak <Vlad Kunak>
2016-12-06 17:26:02 +0200

    fix subscribe

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  // $('.dropdown__state').click(function () {
-  //   console.log('15');
-  //   $('.dropdown__list').toggleClass("active","active");
-  // });
+  $('.dropdown__state').click(function () {
+    console.log('15');
+    $('.dropdown__list').toggleClass("active","active");
+  });
     $('.subscribe__cat').select2({
       minimumResultsForSearch: Infinity
     });
