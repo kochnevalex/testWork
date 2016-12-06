f315c6b5eef4b734a6cf784c00b22b26615245d2
Alex <kochnevalex@gmail.com>
2016-12-06 09:44:21 +0200

    rater

--- assets/js/test.js
+++ assets/js/test.js
@@ -6,5 +6,13 @@ $(document).ready(function () {
     $('.subscribe__cat').select2({
       minimumResultsForSearch: Infinity
     });
+
+
+
+    $('.rater').rate({
+        max_value: 5,
+        step_size: 0.5,
+        initial_value: 3.5});
+
 });
 
