99d4fccae41a4ccafa11c2b3cae25b15324b6391
ottomen <a.shevchenko@makebecool.com>
2016-12-08 16:09:53 +0200

    added updates

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('.dropdown__state').click(function () {
-    console.log('15');
     $('.dropdown__list').toggleClass("active","active");
   });
     $('.subscribe__cat').select2({
